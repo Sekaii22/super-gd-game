b675320508fd704786e28f2886da9fcb7771ff44
Krei-ZL <coolingcoolman@gmail.com>
2024-10-03 01:35:58 +0800

    Animations
    Added new animations for both player and enemies

--- Assets/Scripts/killzone.gd
+++ Assets/Scripts/killzone.gd
@@ -5,3 +5,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("You take " + str(damage) + " damage!")
+		body.health -= damage
+		if body.health <= 0:
+			print("dead")
