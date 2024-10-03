f4c3d629a200415f8cab5bea245fae29b58c1769
Sekaii22 <shawn.ng22@gmail.com>
2024-10-03 18:39:33 +0800

    Small update

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -16,32 +16,36 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	# direction = -1, 0, 1
 	var direction := Input.get_axis("move-left", "move-right")
 	
+	# Flip direction
 	if direction == -1:
 		animated_sprite.flip_h = true
 	elif direction == 1:
 		animated_sprite.flip_h = false
 	
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	# Animation
+	if animated_sprite.animation == "attack" and animated_sprite.is_playing():
+		if is_on_floor():
+			velocity.x = 0
+	else:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-		#Jump animation is temporary
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+			animated_sprite.play("jump")
+			#Jump animation is temporary
+			
+		# Movement
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
 	if Input.is_action_just_pressed("attack"):
-		print("attack pressed")
 		animated_sprite.play("attack")
-
+	
 	move_and_slide()
