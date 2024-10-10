3fc07dde1b28014ed54ccda09a71ae3efec7d7b6
Krei-ZL <coolingcoolman@gmail.com>
2024-10-10 14:58:44 +0800

    CharacterBody2D change
    trying to change node2d to characterbody

--- Assets/Scripts/States/enemy_run.gd
+++ Assets/Scripts/States/enemy_run.gd
@@ -2,9 +2,10 @@ extends State
 class_name EnemyRun
 
 var from_run: bool = false
+var begin_storing: bool = false
 var direction
 var player
-var vel = Vector2()
+var gravity = Vector2(0, 980)
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var enemy: Node2D = $"../.."
 
@@ -21,11 +22,21 @@ func physics_update(_delta: float):
 	#vel = Vector2(direction.x, 0) #needs to be tweaked
 	#enemy.translate(vel * enemy.SPEED)
 	enemy.position.x += direction.x / enemy.SPEED
+	print("enemy y position is " +str(enemy.position.y))
 	#enemy.velocity.x = direction * enemy.SPEED #not working I'm assuming because velocity on Node2D doesnt work
 	#check y position and determine whether enemy should jump
 	if direction.y < 0: # less than 0 for upwards direction
 		enemy.position.y += direction.y / enemy.SPEED
-
+	
+func store_last_direction():
+	if begin_storing == true:
+		var direction2 = player.position - enemy.position
+		begin_storing = false
+		return direction2
+		
+func jump():
+	$"../../Timer".start()
+	
 
 func _on_enemy_damage_taken() -> void:
 	from_run = true
