ac57c5b375a6831f43cc58b41e1b53ef242cb86f
Sekaii22 <shawn.ng22@gmail.com>
2024-10-24 23:50:46 +0800

    Updated stage1_chunk3

--- Assets/Scripts/States/player_jump.gd
+++ Assets/Scripts/States/player_jump.gd
@@ -6,9 +6,9 @@ var player: CharacterBody2D
 @onready var jump_particle: GPUParticles2D = $"../../DoubleJumpParticle"
 
 # TODO: Put all these in a player class resource
-@export var min_time_in_jump: float = 0.2
+#@export var min_time_in_jump: float = 0.2
 
-var jump_timer: float = 0
+#var jump_timer: float = 0
 var var_jump_factor: float = 2.0
 
 func enter():
@@ -23,24 +23,28 @@ func enter():
 		jump_particle.restart()
 		
 	player.jumps_left -= 1
-	jump_timer = min_time_in_jump
+	#jump_timer = min_time_in_jump
 	
 func exit():
 	pass
 	
 func update(_delta: float):
-	if jump_timer > 0:
-		jump_timer -= _delta
+	pass
+	#if jump_timer > 0:
+		#jump_timer -= _delta
 	
 func physics_update(_delta: float):
 	var direction := Input.get_axis("move-left", "move-right")
 	player.velocity.x = direction * player.SPEED
 	
-	# Variable jump
+	# Variable jump for the first jump
 	if Input.is_action_just_released("jump") and player.jumps_left == player.no_of_jumps - 1:
 		player.velocity.y /= var_jump_factor
 	
-	if player.velocity.y >= 0:
+	if InputBuffer.is_action_press_buffered("jump") and player.can_jump():
+		Transition.emit(self, "jump")
+	
+	elif player.velocity.y >= 0:
 		Transition.emit(self, "fall")
 			
 	elif InputBuffer.is_action_press_buffered("attack"):
@@ -48,10 +52,7 @@ func physics_update(_delta: float):
 		
 	elif InputBuffer.is_action_press_buffered("dash") and player.can_dash():
 		Transition.emit(self, "dash")
-		
-	if jump_timer <= 0:
-		if InputBuffer.is_action_press_buffered("jump") and player.can_jump():
-			Transition.emit(self, "jump")
+	
 
 func _on_player_damage_taken() -> void:
 	Transition.emit(self, "hurt")
